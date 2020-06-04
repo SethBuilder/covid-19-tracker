ad455b45ccea44990e502718cadf23a878a7c0e4
Seif <seif@seif.rocks>
2020-06-04 08:07:42 +0300

    footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <>
+        <App />
+        <section className="footer1 cid-qEsj1x8oQy" id="footer1-c">
+            <div className="container">
+                <div className="mbr-col-sm-12 align-center mbr-white">
+                    <p className="mbr-text mbr-fonts-style display-7" style={{ fontSize: '12px', lineHeight: '20px' }}> Built by <a target="_blank" rel="noopener noreferrer" href="https://seif.rocks">Seif Elmughrabi</a> using react.js and chart.js and hosted on Netlify</p>
+                </div>
+
+            </div>
+        </section>
+    </>, document.getElementById("root"));
